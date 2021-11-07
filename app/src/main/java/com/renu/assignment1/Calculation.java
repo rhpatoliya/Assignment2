3d3e16e8a35796ab2f833a81835811d266a18ccb
ywca2021 <asodaria.renuka19@gmail.com>
2021-11-07 13:30:42 -0500

    this is final project

--- app/src/main/java/com/renu/assignment1/Calculation.java
+++ app/src/main/java/com/renu/assignment1/Calculation.java
@@ -1,11 +1,69 @@
 package com.renu.assignment1;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+
 public class Calculation {
+    public static String TAG="Calculation";
+    static ArrayList<String> calculation = new ArrayList<String>();
 
+    public static float CalculatetheValue() {
+        float total_calculation=0;
+        ArrayList<String> tempdata=new ArrayList<>();
+        tempdata.addAll(calculation);
 
-    int cal(int firstNUm, int secondNUm, int opr) {
+        if(tempdata!=null && tempdata.size()>0){
+            for(int i=0;i<tempdata.size();i++){
+                if(tempdata.get(i).contains("+") || tempdata.get(i).contains("-")
+                        || tempdata.get(i).contains("*") || tempdata.get(i).contains("/")  ){
+                    float firstnumber=Float.parseFloat(tempdata.get(i-1));
+                    String operation=tempdata.get(i);
+                    float secondnumber=Float.parseFloat(tempdata.get(i+1));
+                    total_calculation=MethodforCalculation(firstnumber,secondnumber,operation);
+                    tempdata.set(i+1,total_calculation+"");
+                    if(i+1==tempdata.size()){
+                        return total_calculation;
+                    }
+                    i=i+1;
 
-        return firstNUm;
+                }
+            }
+        }
+        return total_calculation;
     }
 
+    private static float MethodforCalculation(float firstnumber, float secondnumber, String operation) {
+        float result=0;
+        switch (operation){
+            case "+":
+                result=firstnumber+secondnumber;
+                Log.e(TAG, "result="+result);
+                break;
+            case "-":
+                result=firstnumber-secondnumber;
+                Log.e(TAG, "result="+result);
+                break;
+            case "*":
+                result=firstnumber*secondnumber;
+                Log.e(TAG, "result="+result);
+                break;
+            case "/":
+                result=firstnumber/secondnumber;
+                Log.e(TAG, "result="+result);
+                break;
+
+        }
+        return result;
+    }
+
+    public static void push(String s) {
+        calculation.add(s);
+    }
+
+    public static void clearArray() {
+        calculation.clear();
+        calculation=new ArrayList<>();
+    }
 }
+
--- app/src/main/java/com/renu/assignment1/MainActivity.java
+++ app/src/main/java/com/renu/assignment1/MainActivity.java
@@ -3,12 +3,16 @@ package com.renu.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    String TAG="MainActivity";
     TextView result , advance_title;
 
     Button one_btu , two_btu , three_btu , four_btu , five_btu , six_btu, seven_btu ,
@@ -18,13 +22,112 @@ public class MainActivity extends AppCompatActivity {
 
     Button equal_btu , plus_btu , minus_btu , time_btu , divided_btu;
 
-    float mValueOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setViewmethod();
+        setClicklistner();
+    }
 
+    private void setClicklistner() {
+        one_btu.setOnClickListener(this);
+        two_btu.setOnClickListener(this);
+        three_btu.setOnClickListener(this);
+        four_btu.setOnClickListener(this);
+        five_btu.setOnClickListener(this);
+        six_btu.setOnClickListener(this);
+        seven_btu.setOnClickListener(this);
+        eight_btu.setOnClickListener(this);
+        nine_btu.setOnClickListener(this);
+        zero_btu.setOnClickListener(this);
+        equal_btu.setOnClickListener(this);
+        minus_btu.setOnClickListener(this);
+        time_btu.setOnClickListener(this);
+        divided_btu.setOnClickListener(this);
+        plus_btu.setOnClickListener(this);
+        clear_btu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.one_btu:
+                result.setText(result.getText() + "1");
+                Calculation.push("1");
+                break;
+            case R.id.two_btu:
+                result.setText(result.getText() + "2");
+                Calculation.push("2");
+                break;
+            case R.id.three_btu:
+                result.setText(result.getText() + "3");
+                Calculation.push("3");
+                break;
+            case R.id.four_btu:
+                result.setText(result.getText() + "4");
+                Calculation.push("4");
+                break;
+            case R.id.five_btu:
+                result.setText(result.getText() + "5");
+                Calculation.push("5");
+                break;
+            case R.id.six_btu:
+                result.setText(result.getText() + "6");
+                Calculation.push("6");
+                break;
+            case R.id.seven_btu:
+                result.setText(result.getText() + "7");
+                Calculation.push("7");
+                break;
+            case R.id.eight_btu:
+                result.setText(result.getText() + "8");
+                Calculation.push("8");
+                break;
+            case R.id.nine_btu:
+                result.setText(result.getText() + "9");
+                Calculation.push("9");
+                break;
+            case R.id.zero_btu:
+                result.setText(result.getText() + "0");
+                Calculation.push("0");
+                break;
+            case R.id.plus_btu:
+                result.setText(result.getText() + "+");
+                Calculation.push("+");
+                break;
+            case R.id.minus_btu:
+                result.setText(result.getText() + "-");
+                Calculation.push("-");
+                break;
+            case R.id.time_btu:
+                result.setText(result.getText() + "*");
+                Calculation.push("*");
+                break;
+            case R.id.divided_btu:
+                result.setText(result.getText() + "/");
+                Calculation.push("/");
+                break;
+            case R.id.clear_btu:
+//                String s=result.getText().toString();
+//                s = s.substring(0,s.length()-1);
+//                result.setText(s);
+                Calculation.clearArray();
+                result.setText("");
+                break;
+            case R.id.equal_btu:
+                // result.setText(result.getText() + "=");
+                float value=Calculation.CalculatetheValue();
+                Calculation.clearArray();
+                result.setText(value+"");
+                Log.e(TAG,"Total calculation is="+value);
+                break;
+        }
+    }
+
+    private void setViewmethod() {
         one_btu = (Button) findViewById(R.id.one_btu) ;
         two_btu = (Button) findViewById(R.id.two_btu);
         three_btu = (Button) findViewById(R.id.three_btu);
@@ -47,122 +150,5 @@ public class MainActivity extends AppCompatActivity {
 
         result = (TextView) findViewById(R.id.result);
         advance_title = (TextView) findViewById(R.id.advance_title);
-
-        one_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "1");
-            }
-        });
-        two_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "2");
-            }
-        });
-        three_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "3");
-            }
-        });
-        four_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "4");
-            }
-        });
-        five_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "5");
-            }
-        });
-        six_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "6");
-            }
-        });
-        seven_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "7");
-            }
-        });
-        eight_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "8");
-            }
-        });
-        nine_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "9");
-            }
-        });
-        zero_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "0");
-            }
-        });
-
-        plus_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "+");
-
-                }
-
-        });
-        minus_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "-");
-
-            }
-
-        });
-        time_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "*");
-
-            }
-
-        });
-
-        divided_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "/");
-
-            }
-
-        });
-
-        equal_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText(result.getText() + "=");
-
-            }
-
-        });
-
-        clear_btu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String s=result.getText().toString();
-                s = s.substring(0,s.length()-1); 
-                result.setText(s);
-            }
-        });
-
-
-
-
     }
 }
